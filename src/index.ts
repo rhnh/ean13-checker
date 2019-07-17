384040a6c72189cde5d75282984c970452fe17d7
rhn <rhnh@protonmail.com>
2019-07-17 13:58:05 +0200

    on focus

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   let input = document.querySelector("input").value;
   let displayResult = document.querySelector("h3");
+
   if (input.length !== 13) {
     return (displayResult.textContent =
       "A valid EAN-13 number should be 13 digits");
   }
+
   let prep = utils.prep(input);
   let last = prep[prep.length - 1];
-  prep.pop();
+  prep.pop(); //<- remove the last digit
   let sum = utils.EAN(prep);
   let result = sum === last ? "correct" : "wrong";
   displayResult.className = result;
