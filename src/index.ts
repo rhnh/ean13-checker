7bcac01916bd54567b500eb486bc7fcae00e246c
rhn <rhnh@protonmail.com>
2019-07-17 12:36:41 +0200

    basic things

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,16 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   let input = document.querySelector("input").value;
-  let result = document.querySelector("h3");
+  let displayResult = document.querySelector("h3");
   if (input.length !== 13) {
-    return (result.textContent = "A valid EAN-13 number should be 13 digits");
+    return (displayResult.textContent =
+      "A valid EAN-13 number should be 13 digits");
   }
   let prep = utils.prep(input);
   let last = prep[prep.length - 1];
   prep.pop();
   let sum = utils.EAN(prep);
-  let style = sum === last ? "✓ correct" : "𐄂 wrong";
-  result.textContent = style;
-  result.style.color = style === "✓ correct" ? "#80c780" : "#f9f9f9";
-  result.style.backgroundColor = "rgba(0,0,0,0.2)";
-  result.style.padding = "2em";
-  result.style.borderRadius = "0.2em";
-  result.style.textTransform = "Capitalize";
+  let result = sum === last ? "correct" : "wrong";
+  displayResult.className = result;
+  displayResult.textContent = result;
 });
